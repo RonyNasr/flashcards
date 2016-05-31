50b6a2a29929799eb3d974f0d566fa4438254076
Carl Egbert and Rony Nasr <carl@>
2016-05-31 16:15:39 -0700

    make it so that only one flashcard is active at a time:

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
-var active="";
+var active;
+var previous = "";
 
 $(document).ready(function(){
   $(".flashcard").click(function(){
+    active=this.id;
     $("#"+this.id + " .term").toggle();
     $("#"+this.id + " .definition").toggle();
-    togglePrevious(active);
-     //active=this.id;
-     //alert("toggled");
+    togglePrevious(active,previous);
+    previous=this.id;
+     //alert("active toggled");
   });
 });
 
 
-var togglePrevious = function(previous){
-if (previous !== "" && previous !== this.id){
+var togglePrevious = function(active,previous){
+if (previous !== "" && previous !== active){
   $("#"+previous + " .term").toggle();
   $("#"+previous + " .definition").toggle();
-  //alert("toggled");
+//  alert("prev toggled");
 
   }
 }
