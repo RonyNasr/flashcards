e713cf7757ff21b6fec72a355a352e5e6cb623f9
Carl Egbert and Rony Nasr <rony@>
2016-05-31 16:02:15 -0700

    add definitions to flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
-
-
+var active="";
 
 $(document).ready(function(){
   $(".flashcard").click(function(){
     $("#"+this.id + " .term").toggle();
     $("#"+this.id + " .definition").toggle();
+    togglePrevious(active);
+     //active=this.id;
+     //alert("toggled");
   });
 });
+
+
+var togglePrevious = function(previous){
+if (previous !== "" && previous !== this.id){
+  $("#"+previous + " .term").toggle();
+  $("#"+previous + " .definition").toggle();
+  //alert("toggled");
+
+  }
+}
